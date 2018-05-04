adc723fc65866b05aedd36e9930bfaf9ad43e31e
Hannah Werman <hannah.werman@gmail.com>
2018-05-04 12:09:23 -0500

    connected to database with activerecord

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -5,6 +5,11 @@ class ApplicationController < Sinatra::Base
 	require 'bundler'
 	Bundler.require()
 
+	ActiveRecord::Base.establish_connection(
+		:adapter => 'postgresql',
+		:database => 'turtle_item'
+	)
+
 	get '/' do
 		'hello'
 		# @message = "here is a message"
